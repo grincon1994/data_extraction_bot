5691912db08a31e75909ae91ac4b5651240c68d2
Gilberto Rincon <grincon1994@gmail.com>
2024-10-21 19:06:16 -0400

    fixed strip on home_prices list

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import re
 import time
 
 form_link = "https://docs.google.com/forms/d/e/1FAIpQLSe8HZhkj8H8ILlO3UqOA9zgSQLaI-yQaRNX6cabNpL9q9xorQ/viewform?usp=sf_link"
+google_form = "https://docs.google.com/forms/d/1J0ctxcOegt0RWsteOFawPFOvZmJOf90BgRzdNVuKeH0/edit#responses"
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
@@ -35,7 +36,7 @@ for link in links:
 for price in prices:
     list_prices_text = price.get_text().strip()
     if '$' in list_prices_text:
-        home_prices.append(list_prices_text.strip('+ /mo'))
+        home_prices.append(list_prices_text.strip('+ /mo 1 bd'))
 
 for address in addresses:
     address_text = address.get_text().strip()
@@ -44,22 +45,28 @@ for address in addresses:
         clean_address = ','.join(part.strip() for part in address_parts).replace('|', '')
         home_address.append(clean_address)
 
+print(home_prices)
 
-#Filling Google Form
-
-address_form = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
-address_form.send_keys("hola")
-
-time.sleep(2)
-
-monthly_price_form = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
-monthly_price_form.send_keys("nena")
-
-time.sleep(2)
-
-property_link_form = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
-property_link_form.send_keys("bro")
 # print(home_links)
-# print(home_prices)
 # print(home_address)
+
+# #Filling Google Form
+
+# for i in range(len(home_links)):
+#     address_space = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
+#     price_space = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
+#     link_space = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
+#     submit_btn = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div/span')
+
+#     address_space.send_keys(home_address[i])
+#     time.sleep(2)
+#     price_space.send_keys(home_prices[i])
+#     time.sleep(2)
+#     link_space.send_keys(home_links[i])
+#     time.sleep(2)
+#     submit_btn.click()
+#     time.sleep(5)
+#     submit_another_answer = driver.find_element(By.XPATH, '/html/body/div[1]/div[2]/div[1]/div/div[4]/a')
+#     submit_another_answer.click()
+
 driver.quit()
